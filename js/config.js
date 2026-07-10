01fc6b203d15dd620630e1ec0952a43ee7f79066
PanzXx <apripanji22@gmail.com>
2026-07-10 22:26:50 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -33,10 +33,10 @@ export const ADMIN_PIN = "121230";
 // Lalu copy direct link MP4-nya
 
 //export const VIDEO_URL = "https://ia601805.us.archive.org/9/items/d-21-fun-lust-caution-2007/D21_FUN-Lust-caution-2007.mp4";
-export const VIDEO_URL = "https://ia601502.us.archive.org/18/items/d-21-fun-son-mask-2005/D21_FUN-Son-mask-2005.mp4";
+export const VIDEO_URL = "https://ia601804.us.archive.org/4/items/family-switch-2023-webdl_X_c9a477a7/family-switch-2023-webdl_X_c9a477a7.mp4";
 
 // Nama film (untuk ditampilkan di UI)
-export const MOVIE_TITLE = "Sayang Zizah";
+export const MOVIE_TITLE = "Family Switch";
 
 // Room ID (ubah jika ingin sesi baru)
 export const ROOM_ID = "2032023";
